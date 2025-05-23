5e3e705a208c043fcff0c4ae93a8f336ca6f3b38
phongnn <ngophong115522@gmail.com>
2025-05-23 15:34:05 +0700

    cap nhat lai du lieu db

--- src/main/java/com/example/demo/Database/Untitled1.sql
+++ src/main/java/com/example/demo/Database/Untitled1.sql
@@ -2,18 +2,46 @@ create database demo_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 show databases;
 use demo_db;
 create table User(
-	id bigint primary key auto_increment,
-    nameUser varchar(100),
-    age int,
-    gender TINYINT,
-    address varchar(250),
-	birthdate DATE
+                     id bigint primary key auto_increment,
+                     nameUser varchar(100),
+                     age int,
+                     gender TINYINT,
+                     address varchar(250),
+                     birthdate DATE
 
 );
-INSERT INTO User (nameUser, age, gender, address, birthdate) VALUES 
-('Nguyễn Văn A', 22, 1, 'Hà Nội', '2002-05-15'),
-('Trần Thị B', 24, 0, 'TP. Hồ Chí Minh', '2000-08-21'),
-('Lê Văn C', 30, 1, 'Đà Nẵng', '1994-12-05'),
-('Phạm Thị D', 28, 0, 'Cần Thơ', '1996-03-17'),
-('Hoàng Văn E', 19, 1, 'Hải Phòng', '2005-11-02');
-select * from User
+INSERT INTO User (nameUser, age, gender, address, birthdate) VALUES
+                                                                 ('Nguyễn Văn A', 22, 1, 'Hà Nội', '2002-05-15'),
+                                                                 ('Trần Thị B', 24, 0, 'TP. Hồ Chí Minh', '2000-08-21'),
+                                                                 ('Lê Văn C', 30, 1, 'Đà Nẵng', '1994-12-05'),
+                                                                 ('Phạm Thị D', 28, 0, 'Cần Thơ', '1996-03-17'),
+                                                                 ('Hoàng Văn E', 19, 1, 'Hải Phòng', '2005-11-02');
+select * from User;
+CREATE TABLE product (
+                         product_id INT AUTO_INCREMENT PRIMARY KEY,
+                         product_name VARCHAR(255) NOT NULL,
+                         user_id bigint,
+                         CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+
+create database demo_Read_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+show databases;
+use demo_Read_db;
+create table ReadUser(
+                         id bigint primary key auto_increment,
+                         nameUser varchar(100),
+                         age int,
+                         gender TINYINT,
+                         address varchar(250),
+                         birthdate DATE
+
+);
+INSERT INTO ReadUser (nameUser, age, gender, address, birthdate) VALUES
+                                                                     ('Nguyễn Văn A', 22, 1, 'Hà Nội', '2002-05-15'),
+                                                                     ('Trần Thị B', 24, 0, 'TP. Hồ Chí Minh', '2000-08-21'),
+                                                                     ('Lê Văn C', 30, 1, 'Đà Nẵng', '1994-12-05'),
+                                                                     ('Phạm Thị D', 28, 0, 'Cần Thơ', '1996-03-17'),
+                                                                     ('Hoàng Văn E', 19, 1, 'Hải Phòng', '2005-11-02');
+select * from ReadUser
+
